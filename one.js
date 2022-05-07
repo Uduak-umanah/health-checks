924e16a94f7ccc56c7f9697069219ba0fab0f8fc
uduak <uduakumanah10@gmail.com>
2022-05-07 16:18:07 +0100

    new

---
+++ one.js
@@ -0,0 +1,26 @@
+const value = true;
+
+if (!value) {
+  console.log('the value is false');
+}
+
+var name = 'kelvin';
+
+switch (name) {
+  case ' ':
+    console.log('the string is empty');
+    break;
+  case 'emmanhel':
+    console.log(`you entered ${value}`);
+    break;
+  case 'tunde':
+    console.log(`you entered ${value}`);
+    break;
+  case 'lade':
+    console.log(`the string is ${value}`);
+    break;
+
+  default:
+    console.log('name not in array');
+    break;
+}
