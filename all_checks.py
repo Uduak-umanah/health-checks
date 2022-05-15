92317e88e1470480fcf4e2871bff4e74f7940349
uduak <uduakumanah10@gmail.com>
2022-05-15 10:54:42 +0100

    manupulating even_better_things branch

---
+++ all_checks.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+
+def main():
+ if check_reboot():
+  print("pending reboot")
+  sys.exit(1)
+  print("every thing is ok")
+  sys.exit(0)
+
+ 
+ pass
+
+
+
+ def bbb(args):
+   pass
+      
